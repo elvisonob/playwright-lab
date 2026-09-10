33ffc87860cf526d94dd931990a5958f6a44428f
elvisonob <elvisonob@yahoo.com>
2026-09-10 18:39:46 +0100

    addint timeout

--- tests/auto-waiting.spec.ts
+++ tests/auto-waiting.spec.ts
@@ -1,9 +1,10 @@
 import { expect, test } from '@playwright/test';
 
-test.beforeEach(async ({ page }) => {
+test.beforeEach(async ({ page }, testInfo) => {
   await page.goto('https://playground.bondaracademy.com/');
   await page.getByText('Modal & Overlays').click();
   await page.getByText('Dialog').click();
+  testInfo.setTimeout(testInfo.timeout + 3000);
 });
 
 test('Auto waiting', async ({ page }) => {
@@ -21,17 +22,44 @@ test('Auto waiting', async ({ page }) => {
   expect(dialogHeaderText).toEqual('Friendly reminder');
 });
 
-test('Auto waiting', async ({ page }) => {
+test('Alternative waits', async ({ page }) => {
   const dialogWithDelayForm = page.locator('nb-card', {
     hasText: 'Open Dialog With Delay',
   });
   await dialogWithDelayForm.getByRole('button', { name: '3 seconds' }).click();
-
   const dialogContainer = page.locator('nb-dialog-container');
-  //await dialogContainer.getByRole('button', { name: 'Ok' }).click();
 
-  const dialogHeaderText = await dialogContainer
-    .locator('nb-card-header')
-    .textContent();
-  expect(dialogHeaderText).toEqual('Friendly reminder');
+  //--wait for the element
+  //await dialogContainer.waitFor();
+  //await page.waitForSelector('nb-dialog-container')
+
+  //--wait for API response
+  //await page.waitForResponse('**/delay/*')
+
+  //--wait for load state (NOT RECOMMENDED)
+  //await page.waitForLoadState('networkidle');
+
+  //--hardcoded wait (NEVEVER EVER USE IT. JUST NEVER)
+  // await page.waitForTimeout(3500);
+
+  // const dialogHeaderText = await dialogContainer
+  //   .locator('nb-card-header')
+  //   .allTextContents();
+  // expect(dialogHeaderText).toContain('Friendly reminder');
+  await expect(dialogContainer.locator('nb-card-header')).toHaveText(
+    'Friendly reminder',
+  );
+});
+
+test('Timeouts', async ({ page }) => {
+  test.slow();
+  const dialogWithDelayForm = page.locator('nb-card', {
+    hasText: 'Open Dialog with Delay',
+  });
+  await dialogWithDelayForm.getByRole('button', { name: '3 seconds' }).click();
+  const dialogContainer = page.locator('nb-dialog-container');
+
+  await dialogContainer
+    .getByRole('button', { name: 'Ok' })
+    .click({ timeout: 4000 });
 });
